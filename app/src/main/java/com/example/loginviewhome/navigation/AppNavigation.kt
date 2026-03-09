1626eb86432cbf88e0d8649e96500e246686b918
angelMV31 <angelgabr2_23@alu.uabcs.mx>
2026-03-09 09:28:01 -0700

    Separation of views and navigation

---
+++ app/src/main/java/com/example/loginviewhome/navigation/AppNavigation.kt
@@ -0,0 +1,29 @@
+package com.example.loginviewhome.navigation
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.loginviewhome.screen.Greeting
+import com.example.loginviewhome.screen.LoginScreen
+import com.example.loginviewhome.screen.Register
+
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "welcome"
+    ) {
+        composable("welcome") {
+            Greeting(navController)
+        }
+        composable("login") {
+            LoginScreen(navController)
+        }
+        composable("register") {
+            Register(navController)
+        }
+    }
+}
+
